2f92d80c49b04dec50a00ec5a552c66256425b06
miguel <brondani.miguel@gmail.com>
2022-09-27 17:12:12 -0300

    bluetooth app

--- AppBluetooth/app/src/main/java/com/example/appbluetooth/MainActivity.java
+++ AppBluetooth/app/src/main/java/com/example/appbluetooth/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.appbluetooth;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button botao_cima, botao_baixo, botao_esquerda, botao_direita;
-    private Switch switch1;
+    private Button botao_cima, botao_baixo, botao_esquerda, botao_direita, botao_conexao;
+    public BluetoothAdapter bluetoothAdaptador; //objeto adaptador bluetooth
+
+    private static final int SOLICITADOR_BLUETOOTH = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,30 @@ public class MainActivity extends AppCompatActivity {
         botao_baixo = findViewById(R.id.bai);
         botao_esquerda = findViewById(R.id.esq);
         botao_direita = findViewById(R.id.dir);
-        switch1 = findViewById(R.id.switch1);
+        botao_conexao = findViewById(R.id.conexao);
 
+        bluetoothAdaptador = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdaptador == null){ //confere se o dispositivo tem bluetooth
+            Toast.makeText(getApplicationContext(), "Sem módulo bluetooth disponível", Toast.LENGTH_LONG).show();
+        }
+        else if (!bluetoothAdaptador.isEnabled()){ //se tem bluetooth, ativa se não está ligado
+                Intent ativarBlue = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(ativarBlue, SOLICITADOR_BLUETOOTH);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case SOLICITADOR_BLUETOOTH:
+                if (resultCode == Activity.RESULT_OK){
+                    Toast.makeText(getApplicationContext(), "Bluetooth ativado", Toast.LENGTH_LONG).show();
+                } else{
+                    Toast.makeText(getApplicationContext(), "Bluetooth NÃO ativado, encerrando aplicativo", Toast.LENGTH_LONG).show();
+                    finish(); // ever aula 2, app nao foi encerrado apos nao ativar bluetooth
+                }
+                break;
+        }
     }
 }
