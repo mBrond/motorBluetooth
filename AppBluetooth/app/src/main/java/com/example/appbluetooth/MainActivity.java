cd098ff8884c4f8945e5e2193ed0d5821701f52f
miguel <brondani.miguel@gmail.com>
2022-10-05 13:25:17 -0300

    continuando app, problemas no bluetooth

--- AppBluetooth/app/src/main/java/com/example/appbluetooth/MainActivity.java
+++ AppBluetooth/app/src/main/java/com/example/appbluetooth/MainActivity.java
@@ -10,6 +10,9 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -30,17 +33,14 @@ public class MainActivity extends AppCompatActivity {
     private static final int SOLICITADOR_BLUETOOTH = 1;
     private static final int SOLICITA_CONEXAO = 2;
 
-    ConnectedThread connectedThread;
 
     boolean conexao = false;
 
     private static String MAC = null;
 
-    UUID MEU_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    Thread ConnectedThread;
 
-    Intent data;
-    int requestCode;
-    int resultCode;
+    UUID MEU_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
         bluetoothAdaptador = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdaptador == null) { //confere se o dispositivo tem bluetooth
             Toast.makeText(getApplicationContext(), "Sem módulo bluetooth disponível", Toast.LENGTH_LONG).show();
-            finish();
         } else if (!bluetoothAdaptador.isEnabled()) { //se tem bluetooth, ativa se não está ligado
             Intent ativarBlue = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(ativarBlue, SOLICITADOR_BLUETOOTH);
@@ -82,22 +81,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        botao_baixo.setOnClickListener(View.OnClickListener() {
-
-            public void onClick(View view) {
-
-                if (conexao) {
-                    connectedThread.enviar("B");
-                } else {
-                    Toast.makeText(getApplicationContext(), "Bluetooth não está conectado", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        });
-
-        protected void onActivityResult(requestCode,resultCode,data){
-            //super.onActivityResult(requestCode, resultCode, data);
+    }
+        @Override
+        protected void onActivityResult( int requestCode, int resultCode, Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
             switch (requestCode) {
                 case SOLICITADOR_BLUETOOTH:
                     if (resultCode == Activity.RESULT_OK) {
@@ -119,8 +106,8 @@ public class MainActivity extends AppCompatActivity {
 
                             conexao = true;
 
-                            connectedThread = new ConnectedThread(meuSocket);
-                            connectedThread.start();
+                            //connectedThread = new ConnectedThread(meuSocket);
+                            //connectedThread.start();
 
                             botao_conexao.setText("Desconectar");
 
@@ -136,34 +123,46 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-        private class ConnectedThread extends Thread {
-            private InputStream mnInStream;
-            private OutputStream mnOutStream;
+    }
+
+        class ConnectedThread extends Thread {
+            private final OutputStream mmOutStream;
+            private byte[] mmBuffer; // mmBuffer store for the stream
 
             public ConnectedThread(BluetoothSocket socket) {
                 meuSocket = socket;
                 InputStream tmpIn = null;
                 OutputStream tmpOut = null;
 
+                // Get the input and output streams; using temp objects because
+                // member streams are final.
                 try {
                     tmpIn = socket.getInputStream();
-                    tmpOut = socket.getOutputStream();
                 } catch (IOException e) {
-                }
-                mnInStream = tmpIn;
-                mnOutStream = tmpOut;
-
-            }
-
-            public void enviar(String dadosEnviar) {
-                byte[] msgBuffer = dadosEnviar.getBytes();
 
                 try {
-                    mnOutStream.write(msgBuffer);
+                    tmpOut = socket.getOutputStream();
+                } catch (IOException i) {
+
+                }
+
+                mmOutStream = tmpOut;
+            }
+
+            // Call this from the main activity to send data to the remote device.
+            public void enviar(String dadosEnviar) {
+                byte[] msgBuffer = dadosEnviar.getBytes();
+                try {
+                    mmOutStream.write(msgBuffer);
+
+
                 } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "Error occurred when sending data", Toast.LENGTH_LONG).show();
+
                 }
             }
+                }
         }
+        //}
 
-    }
-}
+
